7c8e515dec6cc05baf77af56519c93d268719be8
Danny Kim <dudkny@gmail.com>
2019-09-18 14:39:02 -0500

    Added dummy friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,19 +6,83 @@
 
 var friends = [
     {
-        "name": "Test Dummy",
-        "photo": "http://siliconvalleyrealtyworld.com/files/2011/09/Crash_Test_Dummy.jpg",
+        "name": "Kanny D.",
+        "photo": "https://avatars1.githubusercontent.com/u/5622650",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Com Truise",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2017/06/14/14-tom-cruise.w700.h700.jpg",
+        "scores": [
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Orack Bamama",
+        "photo": "https://peopledotcom.files.wordpress.com/2017/09/sexiest-man-alive-mouth-2.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Prad Bitt",
+        "photo": "https://photos.laineygossip.com/articles/pitt-cotillard-allied-22nov16-07.jpg",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Tonald Drump",
+        "photo": "https://static.businessinsider.com/image/59381297b1bd5657008b49d7/image.jpg",
         "scores": [
             5,
-            1,
-            4,
-            4,
             5,
-            1,
-            2,
             5,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     }
 ];
